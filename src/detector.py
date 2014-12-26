bb3255126080f1bf0ebd622c4698fa1a26fe0139
Marek Salat <salat.marek42@gmail.com>
2014-12-26 10:37:00 +0100

    presun veci

---
+++ src/detector.py
@@ -0,0 +1,61 @@
+import cv2
+from knn.knn_classification import Classification
+from note_head_detector import NoteHeadDetector
+
+__author__ = 'Marek'
+
+
+def detect(staff_finder, symbol_extractor, image_without_staff_lines):
+    """
+        fixme: pouze vyextrahovana metoda, signatura by mela byt uplne jina. detektor by mel byt asi trida
+            Detector
+                classify (objects:list) -> labels:list
+                train(objects:list, labels:list) nebo train(labeled_objects:list)
+
+            classify funguje asi nejak nasledovne
+                pokud nas detekovaci algoritmus umi i noty tak to proste zdetekuje
+                pokud ne:
+                    zkusi sadu rozdelit na dve kategorie symboly, noty. Na to se muze pouzit nejaky blby zpusob, treba
+                    jako ten co je v note_head_detector (nerika jaka je to nota, jenom jestli to je nota).
+                    klasifikace pak pokracuje zvlast pro noty zvlast pro ostatni symboly
+    """
+    note_detect = NoteHeadDetector(staff_finder.space_height,
+                                   cv2.imread("../resources/black_head.png", cv2.IMREAD_GRAYSCALE))
+    # train KNearestNeighbour
+    knn = Classification()
+    # trying classification
+    i = 0
+    for group in symbol_extractor.bounding_groups:
+        i += 1
+        box = group[0]
+        symbol = image_without_staff_lines[box.bottom:box.top, box.left:box.right]
+        if symbol.shape[0] > knn.ybox or symbol.shape[1] > knn.xbox:
+            refx = -11
+            count = 0
+            listrefx = []
+            for rect in note_detect.heads(symbol):
+                listrefx.append(rect.x)
+            listrefx.sort()
+            for x in listrefx:
+                if abs(refx - x) > 10:
+                    count += 1
+                    refx = x
+            width = symbol.shape[1] / count
+            # print width , count , symbol.shape[1], note_detect.heads(symbol)
+            startx = 0
+            for k in range(width, symbol.shape[1], width):
+                sym = symbol[:, startx:k]
+                what, distance = knn.classify(sym)
+                print 'co,x,y,vzdalenost(0=ok):', what, box.bottom, box.left + startx, distance
+                file_name = what + '_' + str(i) + '_' + str(k)
+                if distance == 0.0:
+                    file_name = 'ok_' + file_name
+                cv2.imwrite("..\\tmp\\" + file_name + ".png", sym, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
+                startx = k
+            continue
+        what, distance = knn.classify(symbol)
+        print 'co,x,y,vzdalenost(0=ok):', what, box.bottom, box.left, distance
+        file_name = what + '_' + str(i)
+        if distance == 0.0:
+            file_name = 'ok_' + file_name
+        cv2.imwrite("..\\tmp\\" + file_name + ".png", symbol, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
--- src/knn/knn_classification.py
+++ src/knn/knn_classification.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import os.path
 
+
 class Classification:
     def __init__(self):
         # box with note
@@ -9,7 +10,8 @@ class Classification:
         self.ybox = 160
         # number pattern and count
         self.xcount = 140
-        self.yvector = ['#','b','1','2','4','8','8s','16','16s','k','o','p','p4','p8','p16','t','pnt','tnc','t2','t3','t4','t34','t68','tc']
+        self.yvector = ['#', 'b', '1', '2', '4', '8', '8s', '16', '16s', 'k', 'o', 'p', 'p4', 'p8', 'p16', 't', 'pnt',
+                        'tnc', 't2', 't3', 't4', 't34', 't68', 'tc']
         self.ycount = len(self.yvector)
         self.knn = cv2.KNearest()
         # train from knn_data.npz prepared file
@@ -17,18 +19,20 @@ class Classification:
             with np.load('knn_data.npz') as data:
                 train = data['train']
                 train_labels = data['train_labels']
-                self.knn.train(train,train_labels)
+                self.knn.train(train, train_labels)
         elif os.path.exists('dataset.png'):
             # train from image dataset.png
-            gray = cv2.imread('dataset.png',  cv2.CV_LOAD_IMAGE_GRAYSCALE)
-            cells = [np.hsplit(row,self.xcount) for row in np.vsplit(gray,self.ycount)]
+            gray = cv2.imread('dataset.png', cv2.CV_LOAD_IMAGE_GRAYSCALE)
+            cells = [np.hsplit(row, self.xcount) for row in np.vsplit(gray, self.ycount)]
             x = np.array(cells)
-            train = x[:,:3*self.xcount/4].reshape(-1,self.xbox*self.ybox).astype(np.float32) # Size = (xcout*ycount/2,xbox*ybox)
-            test = x[:,3*self.xcount/4:self.xcount].reshape(-1,self.xbox*self.ybox).astype(np.float32) # Size = (xcout*ycount/2,xbox*ybox)
+            train = x[:, :3 * self.xcount / 4].reshape(-1, self.xbox * self.ybox).astype(
+                np.float32)  # Size = (xcout*ycount/2,xbox*ybox)
+            test = x[:, 3 * self.xcount / 4:self.xcount].reshape(-1, self.xbox * self.ybox).astype(
+                np.float32)  # Size = (xcout*ycount/2,xbox*ybox)
             k = np.arange(self.ycount)
-            train_labels = np.repeat(k,3*self.xcount/4)[:,np.newaxis]
-            test_labels = np.repeat(k,self.xcount/4)[:,np.newaxis]
-            self.knn.train(train,train_labels)
+            train_labels = np.repeat(k, 3 * self.xcount / 4)[:, np.newaxis]
+            test_labels = np.repeat(k, self.xcount / 4)[:, np.newaxis]
+            self.knn.train(train, train_labels)
             '''
             ret,result,neighbours,dist = self.knn.find_nearest(test,k=5)
             matches = result==test_labels
@@ -40,12 +44,12 @@ class Classification:
             print "ERROR: not found files knn_data.npz or dataset.png need for classifier!"
 
     # classify image
-    def classify(self,img):
+    def classify(self, img):
         image_bw = cv2.bitwise_not(img)
         box = np.zeros([self.ybox, self.xbox], dtype=np.uint8)
         box[:image_bw.shape[0], :image_bw.shape[1]] = image_bw
-        test = box.reshape(-1,self.xbox*self.ybox).astype(np.float32)
-        ret,result,neighbours,dist = self.knn.find_nearest(test,k=3)
+        test = box.reshape(-1, self.xbox * self.ybox).astype(np.float32)
+        ret, result, neighbours, dist = self.knn.find_nearest(test, k=3)
 
         # print ret,result,neighbours,dist
         # print ret,yvector[int(ret)], dist[0][0]
